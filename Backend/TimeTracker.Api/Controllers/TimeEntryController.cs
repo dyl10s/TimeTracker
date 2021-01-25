d2e4b1539abbf033bb18b14c3de7734dfe4a5cab
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-01-25 17:44:25 -0500

    Backend setup

---
+++ Backend/TimeTracker.Api/Controllers/TimeEntryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TimeTracker.Api.Database;
+using TimeTracker.Api.Models;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeEntryController : ControllerBase
+    {
+        MainDb database;
+
+        public TimeEntryController(MainDb db)
+        {
+            database = db;
+        }
+
+        /// <summary>
+        /// Get a list of all time entries
+        /// </summary>
+        /// <returns>A list of all time entries</returns>
+        [HttpGet]
+        public async Task<List<TimeEntry>> Get()
+        {
+            return await database.TimeEntries.ToListAsync();
+        }
+
+
+        /// <summary>
+        /// Create a new time entry
+        /// </summary>
+        /// <param name="newEntry">New time entry to create</param>
+        /// <returns>The time entry that was entered</returns>
+        [HttpPost]
+        public async Task<TimeEntry> Post(TimeEntry newEntry)
+        {
+            database.TimeEntries.Add(newEntry);
+            await database.SaveChangesAsync();
+            return newEntry;
+        }
+    }
+}
---
+++ Backend/TimeTracker.Api/Database/Models/TimeEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeTracker.Api.Models
+{
+    public class TimeEntry
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set;}
+        public DateTime EndTime { get; set; }
+        public string Message { get; set;}
+    }
+}
