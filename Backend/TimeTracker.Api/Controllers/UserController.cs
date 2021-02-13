974a89e9b5a8803449d2859f65910e1082bcc3f7
justin walter <jwaltt55@gmail.com>
2021-02-12 20:03:31 -0500

    added setpassword endpoint and updated getprofile

--- Backend/TimeTracker.Api/Controllers/UserController.cs
+++ Backend/TimeTracker.Api/Controllers/UserController.cs
@@ -1,41 +1,84 @@
-using Microsoft.AspNetCore.Mvc;        // ControllerBase
-using System.Threading.Tasks;          // Task
-using TimeTracker.Api.DTOs;            // UserDTO
-using TimeTracker.Api.Database;        // MainDb
-using Microsoft.EntityFrameworkCore;   // .AsNoTracking()
-using System.Linq;                     // .Where()
+using Microsoft.AspNetCore.Mvc;           // ControllerBase
+using System.Threading.Tasks;             // Task
+using TimeTracker.Api.DTOs;               // UserDTO
+using TimeTracker.Api.Database;           // MainDb
+using TimeTracker.Api.Database.Models;    // User
+using Microsoft.EntityFrameworkCore;      // .AsNoTracking()
+using System.Linq;                        // .Where()
+using TimeTracker.Api.Helpers;            // AuthHelper
+using Microsoft.AspNetCore.Authorization; // [Authorize]
+using Microsoft.Extensions.Configuration; // IConfiguration
+using System.Collections.Generic;         // List<>
 
 namespace TimeTracker.Api.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
     public class UserController : ControllerBase {
 
         MainDb database;
-
-        public UserController(MainDb database) {
+        AuthHelper authHelper;
+        IConfiguration configuration;
+        int currentUserID;
+        
+        public UserController(MainDb database, AuthHelper authHelper, IConfiguration configuration) {
             this.database = database;
+            this.authHelper = authHelper;
+            this.configuration = configuration;
+            currentUserID = authHelper.GetCurrentUserId(User);
         }
 
-        [HttpGet("{id}")]
-        public async Task<GenericResponseDTO<UserDTO>> Get(int id) {
+        [HttpGet]
+        public async Task<GenericResponseDTO<ProfileDTO>> GetUserProfile() {
 
-            var queryResult = await database.Users.AsNoTracking()
-                .Where(user => user.Id == id)
-                .FirstOrDefaultAsync();
-
+            User queryResult = await database.Users
+                .AsNoTracking()
+                .Include(x => x.Projects)
+                .FirstOrDefaultAsync(user => user.Id == currentUserID);
+            
             if(queryResult == default(Database.Models.User)) {
-                return new GenericResponseDTO<UserDTO> {
+                return new GenericResponseDTO<ProfileDTO> {
                     Message = "No matching User ID found.",
                     Success = false
                 };
             }
 
-            return new GenericResponseDTO<UserDTO>() {
-                Data = new UserDTO() {
+            return new GenericResponseDTO<ProfileDTO>() {
+                Data = new ProfileDTO() {
                     Email = queryResult.Email,
-                    Name = queryResult.Name
+                    Name = queryResult.Name,
+                    Projects = queryResult.Projects
+                        .Select(x => x.Name)
+                        .ToList()
                 },
                 Success = true
             };
+
         }
+
+        [HttpPost]
+        [Route("/SetPassword")]
+        public async Task<GenericResponseDTO<int>> SetPassword(string password) {
+
+            User queryResult = await database.Users
+                .AsQueryable()
+                .FirstOrDefaultAsync(user => user.Id == currentUserID);
+
+            if(queryResult == default(Database.Models.User)) {
+                return new GenericResponseDTO<int> {
+                    Message = "No matching User ID found.",
+                    Success = false
+                };
+            }
+
+            queryResult.Password = authHelper.GetPasswordHash(password, configuration);
+            database.SaveChanges();
+
+            return new GenericResponseDTO<int> {
+                Success = true
+            };
+        }
+
     }
-    
 }
