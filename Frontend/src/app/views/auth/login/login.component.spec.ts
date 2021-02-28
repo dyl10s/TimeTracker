d165f6a5292b1749eabd2e8dd06f6d55aefafdf3
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-02-28 16:15:49 -0500

    Fixed test

--- Frontend/src/app/views/auth/login/login.component.spec.ts
+++ Frontend/src/app/views/auth/login/login.component.spec.ts
@@ -1,17 +1,27 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { tokenGetter } from 'src/app/app.module';
+import { AuthApiService } from 'src/app/core/services/auth/auth-api.service';
 import { LoginComponent } from './login.component';
-
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      providers: [AuthApiService],
+      imports: [
+        HttpClientModule, 
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: tokenGetter
+          }
+        }),
+        AppRoutingModule]
     })
     .compileComponents();
   }));
