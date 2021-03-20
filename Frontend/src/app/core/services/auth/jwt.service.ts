61a8f0be089d3ab52ff4ff6d33b8ba4ddef8a701
Adam Chlebek <adamchlebek@live.com>
2021-03-19 19:50:23 -0600

    updated refresh

--- Frontend/src/app/core/services/auth/jwt.service.ts
+++ Frontend/src/app/core/services/auth/jwt.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { PayloadDTO } from '../../models/auth/PayloadDto.model';
+import { GenericResponseDTO } from '../../models/GenericResponseDTO.model';
+import { AuthApiService } from './auth-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor(public jwtHelper: JwtHelperService) {}
+  constructor(
+    public jwtHelper: JwtHelperService,
+    private authApiService: AuthApiService
+  ) {}
 
   public setTokens(jwt: string, refresh: string) {
     localStorage.setItem('accessToken', jwt);
@@ -26,11 +31,28 @@ export class JwtService {
     return localStorage.getItem('accessToken');
   }
 
+  public getRefreshToken(){
+    return localStorage.getItem('refreshToken');
+  }
+
   public isAuthenticated() {
     const accessToken = localStorage.getItem('accessToken');
 
     try {
-      return !this.jwtHelper.isTokenExpired(accessToken);
+      if(this.jwtHelper.isTokenExpired(accessToken)){
+        this.authApiService.refresh({ email: this.decode().email, refreshToken: this.getRefreshToken() }).subscribe((response: GenericResponseDTO) => {
+          if(response.success) {
+            this.setTokens(response.data.accessToken, response.data.refreshToken);
+          }else{
+            return false;
+          }
+          return true;
+        }, (err) => {
+          return false;
+        })
+      } else {
+        return true;
+      }
     } catch {
       return false;
     }
