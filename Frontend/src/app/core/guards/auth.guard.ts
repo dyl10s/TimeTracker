a5106b481615d13bba2960c700e13273b988e238
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-04-03 01:49:40 -0400

    Added refresh token logic

--- Frontend/src/app/core/guards/auth.guard.ts
+++ Frontend/src/app/core/guards/auth.guard.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { NbToastrService } from '@nebular/theme';
@@ -40,6 +41,30 @@ export class AuthGuard implements CanActivate {
     if (this.jwtService.isAuthenticated()) {
       // logged in so return true
       return true;
+    }
+
+    // Check if the token is expired
+    if(this.jwtService.isExpired()) {
+      return this.jwtService.refreshToken().pipe(map((res) => {
+
+        // If there was an error refreshing the token
+        if(res.success == false) {
+          this.jwtService.logout(); // Clear bad token
+          this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
+          return false; 
+        }
+
+        if(next.queryParams.inviteCode){
+          this.projectService.addUserToProject(next.queryParams.inviteCode).subscribe((response: GenericResponseDTO) => {
+            this.toastrService.success("You have been added to a project", "Success")
+            this.router.navigate(['/dashboard/profile']);
+            return false;
+          })
+        }
+
+        this.jwtService.setTokens(res.data.accessToken, res.data.refreshToken);
+        return true;
+      }));
     }
 
     // not logged in so redirect to login page with the return url
--- Frontend/src/app/core/services/auth/jwt.service.ts
+++ Frontend/src/app/core/services/auth/jwt.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 import { PayloadDTO } from '../../models/auth/PayloadDto.model';
+import { AuthApiService } from './auth-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor(public jwtHelper: JwtHelperService) {}
+  constructor(public jwtHelper: JwtHelperService, private authService: AuthApiService) {}
 
   public setTokens(jwt: string, refresh: string) {
     localStorage.setItem('accessToken', jwt);
@@ -24,6 +26,26 @@ export class JwtService {
 
   public getJWT(){
     return localStorage.getItem('accessToken');
+  }
+
+  public getRefreshToken(){
+    return localStorage.getItem('refreshToken');
+  }
+
+  public refreshToken() : Observable<any> {
+    return this.authService.refresh({
+      email: this.decode().email,
+      refreshToken: this.getRefreshToken()
+    });
+  }
+
+  public isExpired() {
+    let jwt = this.getJWT();
+    if(jwt) {
+      return this.jwtHelper.isTokenExpired(this.getJWT());
+    }else{
+      return false;
+    }
   }
 
   public isAuthenticated() {
