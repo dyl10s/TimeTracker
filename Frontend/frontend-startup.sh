674555e2799c5b5372efa04712d8fc7a02611fd2
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-04-12 17:54:55 -0400

    progress

--- Frontend/frontend-startup.sh
+++ Frontend/frontend-startup.sh
@@ -1,10 +1,8 @@
 # This file controls what happens to the frontend docker container on startup
-
-#Start Nginx
-nginx -g 'daemon off;'
-
-#Generate the certificate
-certbot --nginx --non-interactive --agree-tos -m strohscd@mail.gvsu.edu -d dev.timetrack.ml
-
+# Generate the certificate after a few seconds
+# waiting so nginx has some time to get started up
+( sleep 5 ; certbot --nginx --non-interactive --agree-tos -m strohscd@mail.gvsu.edu -d *.timetrack.ml ) &
 # Auto renew the certificate
 echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null
+#Start Nginx
+nginx -g 'daemon off;'
