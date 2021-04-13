e8166400453c6fffdbdd8e4e553daf094c6feddb
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-04-13 12:28:16 -0400

    One day this will work!

--- Frontend/frontend-startup.sh
+++ Frontend/frontend-startup.sh
@@ -3,6 +3,6 @@
 # waiting so nginx has some time to get started up
 ( sleep 5 ; certbot --nginx --non-interactive --agree-tos -m strohscd@mail.gvsu.edu -d *.timetrack.ml ) &
 # Auto renew the certificate
-echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null
+( echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null ) &
 #Start Nginx
 nginx -g 'daemon off;'
