b278d4c5b7a26cb1460ef20da76789ebd05d4ad9
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-04-13 13:15:03 -0400

    Use env variables

--- Frontend/frontend-startup.sh
+++ Frontend/frontend-startup.sh
@@ -1,8 +1,8 @@
 # This file controls what happens to the frontend docker container on startup
 # Generate the certificate after a few seconds
 # waiting so nginx has some time to get started up
-( sleep 5 ; certbot --nginx --non-interactive --agree-tos -m strohscd@mail.gvsu.edu -d *.timetrack.ml ) &
+( sleep 5 ; certbot --nginx --non-interactive --agree-tos -m $SSLEmail -d $Domain1 -d $Domain2 ) &
 # Auto renew the certificate
-( echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null ) &
+( echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q && nginx -s reload" | tee -a /etc/crontab > /dev/null ) &
 #Start Nginx
 nginx -g 'daemon off;'
